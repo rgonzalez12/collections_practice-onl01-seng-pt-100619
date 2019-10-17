6e82c0382c05426f093cdbf978f4725092165e3d
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-17 05:26:05 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+ array.sort
+end
 
+def sort_array_desc(array)
+ array.sort.reverse 
+end
+
+def sort_array_char_count(array)
+ array.sort do |left, right|
+  left.length <=> right.length
+ end
+end
+
+def swap_elements(array)
+ array[1], array[2] = array[2], array[1]
+ array
+end
+
+def reverse_array(array)
+ array.reverse
+end
+
+def kesha_maker(array)
+ array.each do |element|
+  element[2] = "$"
+ end
+end
+
+def find_a(array)
+ array.select do |element|
+  element[0] == "a"
+ end
+end
+
+def sum_array(array)
+ array.inject(:+)
+end
+
+def add_s(array)
+ array.collect do |letter|
+  if array[1] == letter
+   letter
+  else
+   letter + "s"
+  end
+ end
+end
